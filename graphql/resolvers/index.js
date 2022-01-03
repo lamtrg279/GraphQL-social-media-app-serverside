cdc43a9495c1bd6e0f7aa09770679dd9edbd092d
Lam <nl2791999@gmail.com>
2022-01-03 10:56:26 -0600

    first commit

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,13 @@
+/** @format */
+
+const postsResolver = require("./post");
+const usersResolver = require("./user");
+
+module.exports = {
+  Query: {
+    ...postsResolver.Query,
+  },
+  Mutation: {
+    ...usersResolver.Mutation,
+  },
+};
---
+++ index.js
@@ -0,0 +1,23 @@
+/** @format */
+
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connect to mongoose");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server listens at port ${res.url}`);
+  });
